558e6151420bfe2c6194b618c1ae7758cff28820
zhangzhe <zhangzhe@bilibili.com>
2016-03-22 14:02:10 +0800

    add terminal

--- scanConfig/init.sh
+++ scanConfig/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-python /www/manage.py collectstatic --noinput
-python /www/manage.py syncdb --noinput
-python /www/manage.py makemigrations
-python /www/manage.py migrate --noinput
-
+chmod -R 777 /www 
+cd /www/ && python /www/manage.py makemigrations
+cd /www/ &&  python /www/manage.py migrate --noinput
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'password')" | python /www/manage.py shell
+
+useradd -g root pocscan
+echo "password" > passwd --stdin pocscan
