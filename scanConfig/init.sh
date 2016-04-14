fc2011153e98e5090adc76d208eb3d70e7d7a685
zhangzhe <zhangzhe@bilibili.com>
2016-04-14 10:44:03 +0800

    pocscan 2.0 +sqlmap

--- scanConfig/init.sh
+++ scanConfig/init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-chmod -R 777 /www 
 cd /www/ && python /www/manage.py makemigrations
 cd /www/ &&  python /www/manage.py migrate --noinput
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'password')" | python /www/manage.py shell
+chmod -R 777 /www 
+echo "root:password"|chpasswd
 
-useradd -g root pocscan
-echo "password" > passwd --stdin pocscan
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'password')" | python /www/manage.py shell
