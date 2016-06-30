a9bea6900585703fb97f70f3ee879a7fa1215c7d
zhangzhe <zhangzhe@bilibili.com>
2016-06-30 10:56:28 +0800

    fix nomoudle named common

---
+++ bugscan/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /www/ && python /www/manage.py makemigrations
+cd /www/ &&  python /www/manage.py migrate --noinput
+
+chmod -R 777 /www 
+echo "root:password"|chpasswd
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'password')" | python /www/manage.py shell
